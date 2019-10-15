a6622212e5677c59e60a3c99aab26c96b2ca23d7
Lambda <lambda@Lambdas-MacBook-Pro-3.local>
2019-10-15 08:52:51 +0100

    attempted queue and stack

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -1,5 +1,8 @@
 import sys
-sys.path.append('../doubly_linked_list')
+# sys.path.append('../doubly_linked_list/')
+import os
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(
+    __file__)), '../doubly_linked_list'))
 from doubly_linked_list import DoublyLinkedList
 
 
@@ -7,13 +10,19 @@ class Queue:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        self.storage = DoublyLinkedList()
 
     def enqueue(self, value):
-        pass
+        self.storage.add_to_tail(value)
+        # pass
 
     def dequeue(self):
-        pass
+        if self.storage.length:
+            return self.storage.remove_from_head()
+        else:
+            return
+        # pass
 
     def len(self):
-        pass
+        return self.storage.length
+        # pass
--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -1,18 +1,27 @@
-from doubly_linked_list import DoublyLinkedList
+# from doubly_linked_list import DoublyLinkedList
 import sys
-sys.path.append('../doubly_linked_list')
+# sys.path.append('../doubly_linked_list')
+import os
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(
+    __file__)), '../doubly_linked_list'))
+from doubly_linked_list import DoublyLinkedList
 
 class Stack:
-    def __init__(self):
+    def __init__(self, node):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        self.storage = DoublyLinkedList()
 
     def push(self, value):
-        pass
+        self.storage.add_to_tail(value)
+        ++self.storage.length
 
     def pop(self):
-        pass
+        if(self.storage.length > 0):
+            self.storage.remove_from_tail()
+            --self.storage.length
+        else:
+            return None
 
     def len(self):
-        pass
+        return self.storage.length
